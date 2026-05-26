18a263f0f4db493f4dbc0731a5d8dff9a54e0253
Тимур <neonix@MacBook-Air-Timur.local>
2026-05-26 14:35:25 +0300

    Roating upload

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,36 +2,15 @@ import '../styles/fonts.css';
 import '../styles/variables.css';
 import '../styles/light.css';
 import '../styles/dark.css';
-// import '../styles/scrollbar.css';
-// import '../styles/animations.css';
-import style from './app.module.css';
+
+import { RouterProvider } from 'react-router-dom'
+import { appRouter } from './appRouter'
 
 import { useTheme } from '../hooks/useTheme';
 
-export const App = () => {
+const App: React.FC = () => {
   useTheme();
+  return <RouterProvider router={appRouter} />
+}
 
-  return (
-    <body className={style.page}>
-      <h1 className={style.heading}>My future CV</h1>
-      <div className={style.fontsGrid}>
-        <ul className={style.fontsList}>
-          <li className= {style.hasklig}>
-            Хасклиг (ru)
-          </li>
-          <li className= {style.firacode}>
-            Фира (ru)
-          </li>
-        </ul>
-        <ul className={style.fontsList}>
-          <li className= {style.hasklig}>
-            Hasklig (en)
-          </li>
-          <li className= {style.firacode}>
-            FiraCode (en)
-          </li>
-        </ul>
-      </div>
-    </body>
-  );
-};
+export default App
